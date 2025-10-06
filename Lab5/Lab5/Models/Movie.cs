b1286b677f857f447c339ac62d249fa6f5272ac6
Bkski <bukowskihubert@gmail.com>
2025-10-06 13:44:50 +0200

    Lab5

---
+++ Lab5/Lab5/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab5.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; }
+
+        [UIHint("LongText")]
+        public string Description { get; set; }
+
+        [Range(0, 5)]
+        [UIHint("Stars")]
+        public int Rating { get; set; }
+
+        [Url]
+        public string TrailerLink { get; set; }
+    }
+}
